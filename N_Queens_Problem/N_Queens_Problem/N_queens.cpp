e4999e0c2b62c9f6ff10cbede3af9c0072ba4c19
Dongliang (Larry) Yi <dongliang.yi@aol.com>
2018-06-27 17:02:17 -0500

    Update N_queens.cpp

--- N_Queens_Problem/N_Queens_Problem/N_queens.cpp
+++ N_Queens_Problem/N_Queens_Problem/N_queens.cpp
@@ -1,11 +1,7 @@
-//
 //  main.cpp
 //  N_Queens_Problem
-//
 //  Created by Dongliang Yi on 9/10/15.
 //  Copyright (c) 2015 Dongliang Yi. All rights reserved.
-//
-
 // N Queens Problem via (Backtracking, which is implemented by) Recursion
 // Written by Prof. Sreenivas for IE523: Financial Computing
 
@@ -15,18 +11,8 @@
 int main (int argc, char * const argv[])
 {
     Board x;
-    
     int board_size;
     sscanf (argv[1], "%d", &board_size); // command line input the N of the problem
-    
-    /*std::cout << endl;
-    
-    std::cout << board_size;
-    
-    std::cout << endl;*/
-
-    
     x.nQueens(board_size);
-    
     return 0;
 }
