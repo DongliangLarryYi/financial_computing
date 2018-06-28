46b99d2cc8af067f1ddf9696aae120f539dd5e92
Dongliang (Larry) Yi <dongliang.yi@aol.com>
2018-06-27 21:13:58 -0500

    Update and rename sudoku_all/sudoku_all/sudoku_all.cpp to sudoku_all/sudoku_all.cpp

--- sudoku_all/sudoku_all.cpp
+++ sudoku_all/sudoku_all.cpp
@@ -1,5 +1,4 @@
-//  main.cpp
-//  sudoku_all
+//  Sudoku Solver via exhaustive search using recursion
 //  Created by Dongliang Yi on 9/14/15.
 //  Copyright (c) 2015 Dongliang Yi. All rights reserved.
 
@@ -12,8 +11,6 @@ int main (int argc, char * const argv[])
     x.read_puzzle(argc, argv);
     x.print_puzzle();
     x.Solve(0,0);
-    //x.alternate_Solve(0, 0);
-    //x.print_puzzle();
     x.print_total();
     return 0;
 }
